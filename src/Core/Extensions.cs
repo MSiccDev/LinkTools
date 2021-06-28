0aefde63c515ea04f74351ce4e880732edea85f4
Marco Siccardi <68176309+MSicc@users.noreply.github.com>
2021-06-28 06:59:17 +0200

    Nuget update, entity separation
    Separated entity classes into own assembly and nuget; update nuget packages

--- src/Core/Extensions.cs
+++ src/Core/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -115,7 +116,80 @@ namespace MSiccDev.Libs.LinkTools
         }
 
 
+        public static string RecursiveHtmlDecode(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return text;
+            var tmp = WebUtility.HtmlDecode(text);
+            while (tmp != text)
+            {
+                text = tmp;
+                tmp = WebUtility.HtmlDecode(text);
+            }
+            return text;
+        }
 
+        public static string RecursiveUrlDecode(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return text;
+            var tmp = WebUtility.UrlDecode(text);
+            while (tmp != text)
+            {
+                text = tmp;
+                tmp = WebUtility.UrlDecode(text);
+            }
+            return text;
+        }
+
+
+        internal const string LinkPattern = @"(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'.,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))";
+
+        public static string[] ToPlainUrls(this string text)
+        {
+            if (string.IsNullOrEmpty(text)) return null;
+
+            var linksInText = Regex.Matches(text, LinkPattern, RegexOptions.Multiline);
+
+            var links = new List<string>();
+
+            foreach (var link in linksInText.Cast<object>().Where(link => !links.Contains(link.ToString())))
+            {
+                links.Add(link.ToString());
+            }
+
+            return links.ToArray();
+        }
+
+        public static string ToPlainTextWithoutUrls(this string text, bool removeOnlyFirst = false)
+        {
+            string cleanedText = "";
+
+            var urls = text.ToPlainUrls();
+
+            if (urls?.Length > 0)
+            {
+                if (removeOnlyFirst)
+                {
+                    var removedLinkText = text.Replace(urls.FirstOrDefault(), "");
+                    cleanedText = removedLinkText.Replace("  ", " ");
+                }
+                else
+                {
+                    foreach (var u in urls)
+                    {
+                        if (text.Contains(u))
+                        {
+                            var removedLinkText = text.Replace(u, "");
+                            cleanedText = removedLinkText.Replace("  ", " ");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                cleanedText = text;
+            }
+            return cleanedText;
+        }
 
     }
 }
